00311a86e66d358c6ec309a9e91d6cb11f13469b
Cool Pidor <giganibik@gmail.com>
2024-05-17 19:35:43 +0300

    1.0.1

--- reflex_runner.py
+++ reflex_runner.py
@@ -1,4 +1,9 @@
 import functions
 import reflex
 
-reflex.Reflex(f'{functions.path}/reflex/main.rx').run(app=functions.create.client(input('Введите имя сессии: ')))
+try:
+    reflex.Reflex(f'{functions.path}/reflex/main.rx').run(app=functions.create.client(input('Введите имя сессии: ')))
+except Exception as e:
+    print(f'{e}\nView documentation on https://github.com/Quadrotez/SharkoGram/blob/master/doc/reflex.MD')
+finally:
+    input()
